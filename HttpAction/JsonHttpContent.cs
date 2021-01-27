3ad186d64a085832e5ccd7f611d47044526f0fe1
AlphaBs <ksi123456ab@naver.com>
2021-01-27 22:33:54 +0900

    create project

---
+++ HttpAction/JsonHttpContent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpAction
+{
+    public class JsonHttpContent : HttpContent
+    {
+        public JsonHttpContent(object obj)
+        {
+
+        }
+
+        protected override Task SerializeToStreamAsync(Stream stream, TransportContext context)
+        {
+            context.
+        }
+
+        protected override bool TryComputeLength(out long length)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
